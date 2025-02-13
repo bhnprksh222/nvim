4a6048893a5c4862be48ee8cbc18e9c015e99f6f
Bhanu Prakash <bhnprksh222@gmail.com>
2025-02-12 22:14:48 -0500

    added customisation

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
-require 'core.options'
-require 'core.keymaps'
+require("core.options")
+require("core.keymaps")
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
+require("lazy").setup({
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
@@ -19,13 +19,20 @@ require('lazy').setup({
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
-		}
+		},
 	},
-	require('plugins.colortheme'),
-	require('plugins.neotree'),
-	require('plugins.bufferline'),
-	require('plugins.lualine'),
-    require('plugins.treesitter'),
-    require('plugins.telescope'),
-    require('plugins.lsp')
+	require("plugins.colortheme"),
+	require("plugins.neotree"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.alpha"),
+	require("plugins.comment"),
+	require("plugins.gitsigns"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+	require("plugins.none-ls"),
 })
--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,4 +1,4 @@
-return { 
+return {
     "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000,
