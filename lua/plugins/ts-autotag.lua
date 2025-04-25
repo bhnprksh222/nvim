266bdc214f669694ec0d63c30d165bb1a8a093ca
Bhanu Prakash <bhnprksh222@gmail.com>
2025-04-25 19:25:05 -0400

    refact

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -1,29 +1,38 @@
 return {
 	"windwp/nvim-ts-autotag",
-	event = { "BufReadPre", "BufNewFile" }, -- Load early for better responsiveness
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		-- Configure nvim-ts-autotag
 		require("nvim-ts-autotag").setup({
 			opts = {
-				enable_close = true, -- Auto close tags when typing '>'
-				enable_rename = true, -- Auto rename when modifying opening/closing tag
-				enable_close_on_slash = false, -- Do not auto-close on typing '</'
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = false,
 			},
-			filetypes = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+			filetypes = {
+				"html",
+				"javascript",
+				"typescript",
+				"javascriptreact",
+				"typescriptreact",
+				"svelte",
+				"vue",
+				"xml",
+			},
 			per_filetype = {
-				["html"] = { enable_close = true }, -- Ensure auto-close works for HTML
+				["html"] = { enable_close = true },
 			},
 		})
 
-		-- Enable live updates when renaming tags
-		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-			underline = true,
+		vim.diagnostic.config({
 			virtual_text = {
 				spacing = 5,
-				severity_limit = "Warning",
+				severity = vim.diagnostic.severity.WARN,
 			},
-			update_in_insert = true, -- Update closing tag while typing
+			underline = true,
+			update_in_insert = true,
 		})
+
+		-- Optional: remap '>' to auto close tag instantly in insert mode
 		vim.api.nvim_set_keymap("i", ">", "><C-x><C-o>", { noremap = true, silent = true })
 	end,
 }
