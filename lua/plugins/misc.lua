ad77154fcac0adccd35aa21e1b354fec858a7d56
Bhanu Prakash <bhnprksh222@gmail.com>
2026-01-02 05:00:47 -0500

    added markdown preview support

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -84,4 +84,14 @@ return {
 			"rcarriga/nvim-notify",
 		},
 	},
+	-- markdown preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
